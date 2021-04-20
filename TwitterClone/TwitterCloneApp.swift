93dc81d7194c3acb32d308cc20d517bd75e43697
Bastien <bastiengiat@gmail.com>
2021-04-20 11:42:38 +0200

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by bastien giat on 20/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
